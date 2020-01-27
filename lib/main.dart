9df4cccc5b9fb33b4504e19bd19759541518e80a
Nabinda <nabindangol2@gmail.com>
2020-01-27 14:38:24 +0545

    Made new Repo for Xylo App

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  int keyNumber;
+  Color keyColor;
+
+  Widget builtKey({int keyNumber, Color keyColor}) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0),
+        child: Container(color: keyColor),
+        onPressed: () {
+          playSound(keyNumber);
+        },
+      ),
+    );
+  }
+
+  void playSound(int keyNumber) {
+    final player = AudioCache();
+    player.play("note$keyNumber.wav");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Xylpohone",
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              builtKey(keyNumber: 1, keyColor: Colors.red),
+              builtKey(keyNumber: 2, keyColor: Colors.orange),
+              builtKey(keyNumber: 3, keyColor: Colors.yellow),
+              builtKey(keyNumber: 4, keyColor: Colors.green),
+              builtKey(keyNumber: 5, keyColor: Colors.green[900]),
+              builtKey(keyNumber: 6, keyColor: Colors.blue),
+              builtKey(keyNumber: 7, keyColor: Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
